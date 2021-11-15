2cab4b7cdabb3aeb4cef76b8483be2159a967e5c
Jens Langhammer <jens.langhammer@beryju.org>
2021-11-15 11:49:08 +0100

    initial
    Signed-off-by: Jens Langhammer <jens.langhammer@beryju.org>

--- authentik/root/settings.py
+++ authentik/root/settings.py
@@ -82,8 +82,9 @@ INSTALLED_APPS = [
     "authentik.sources.oauth",
     "authentik.sources.plex",
     "authentik.sources.saml",
-    "authentik.stages.authenticator",
+    "authentik.sources.scim",
     "authentik.stages.authenticator_duo",
+    "authentik.stages.authenticator",
     "authentik.stages.authenticator_sms",
     "authentik.stages.authenticator_static",
     "authentik.stages.authenticator_totp",
@@ -146,6 +147,9 @@ SPECTACULAR_SETTINGS = {
         "UserTypeEnum": "authentik.core.models.UserTypes",
     },
     "ENUM_ADD_EXPLICIT_BLANK_NULL_CHOICE": False,
+    "PREPROCESSING_HOOKS": [
+        "authentik.api.schema.preprocess_schema_exclude_non_api",
+    ],
     "POSTPROCESSING_HOOKS": [
         "authentik.api.schema.postprocess_schema_responses",
         "drf_spectacular.hooks.postprocess_schema_enums",
